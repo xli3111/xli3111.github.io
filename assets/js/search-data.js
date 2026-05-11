51f4e560457af99588af046b4ecf43f4996e10df
xli3111 <159096249+xli3111@users.noreply.github.com>
2026-05-11 18:57:14 +0000

    Deploying to gh-pages from @ xli3111/xli3111.github.io@a9fb759f74285d0514546aa07f76ac629caf31d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-about",
-          title: "About",
+        },{id: "nav-cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
